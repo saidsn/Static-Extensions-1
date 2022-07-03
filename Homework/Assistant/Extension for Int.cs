b669601d3bdd75a5b1ae8311f78572aad4550bcc
saidsn <saidsn@code.edu.az>
2022-07-04 00:09:31 +0400

    PowerNumbers

---
+++ Homework/Assistant/Extension for Int.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework.Assistant
+{
+    public static class Extension
+    {
+        public static double PowerNumbers(this int n,int m)
+        {
+            return Math.Pow(n, m);
+        }
+    }
+}
---
+++ Homework/Program.cs
@@ -0,0 +1,18 @@
+﻿using Homework.Assistant;
+using System;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 4;
+            num1.PowerNumbers(3);
+            var power = num1.PowerNumbers(2);
+            Console.WriteLine(power);
+            
+
+        }
+    }
+}
